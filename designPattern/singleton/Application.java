c29192d1ebbdd8b08f9013f3658fd87305c1487f
Prajjawal-Prayank <prajjawal.prayank@gmail.com>
2025-10-19 14:30:03 +0530

    added code for singleton

--- designPattern/singleton/Application.java
+++ designPattern/singleton/Application.java
@@ -1,7 +1,16 @@
+package singleton;
+
 public class Application {
 
     public static void main(String args[]) {
-        
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        if(singleton1==singleton2) {
+            System.out.println("Both objects are same. Singleton works.");
+        } else {
+            System.out.println("Both objects are different. Singleton does not work.");
+        }
     }
 
 }
