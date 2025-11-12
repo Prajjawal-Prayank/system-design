defed4152b501175733cebf361c1c2c07f72f514
Prajjawal Prayank <prajjawal.prayank@gmail.com>
2025-11-13 02:12:09 +0530

    added director class

---
+++ designPattern/builder/src/builder/CarDirector.java
@@ -0,0 +1,30 @@
+package builder;
+
+import java.time.LocalDate;
+
+import model.Car;
+
+public class CarDirector {
+    private Car.CarBuilder carBuilder;
+
+    public CarDirector(Car.CarBuilder carBuilder) {
+        this.carBuilder = carBuilder;
+    }
+
+    public Car buildWhiteFiveSeateCar() {
+        return carBuilder
+            .colour("White")
+            .numOfSeats(5)
+            .yearOfManufacture(LocalDate.now().getYear())
+            .build();
+    }
+
+    public Car buildBlack7SeatCar() {
+        return carBuilder
+            .colour("Black")
+            .numOfSeats(7)
+            .yearOfManufacture(LocalDate.now().getYear())
+            .build();
+    }
+
+}
